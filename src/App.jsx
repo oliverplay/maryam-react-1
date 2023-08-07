dcdde30e83d6a3a7adcd8e15b724a034f3520928
Oleg <116948332+oliverplay@users.noreply.github.com>
2023-08-07 20:11:34 +0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Profile from 'components/Profile';
+
+const App = () => {
+  return (
+    <div>
+      App
+      <Profile />
+    </div>
+  );
+};
+
+export default App;
